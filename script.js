f987bd806980d1888262ce9f06631905c4d116cc
ewdwins <saleh.tarkashvand@gmail.com>
2023-07-06 00:19:07 +0330

    add js

---
+++ script.js
@@ -0,0 +1,38 @@
+window.addEventListener("hashchange" , (e)=>{
+    router()
+})
+
+function router(){
+    switch(location.hash){
+        case "" :
+            render({title:"HOME" , color:"red" , href:"#home"})
+            break;
+        case "#home" :
+            render({title:"HOME" , color:"red" , href:"#home"})
+            break;
+        case "#search" :
+            render({title:"SEARCH" , color:"pink" , href:"#search"})
+            break;
+        case "#likes" :
+            render({title:"LIKES" , color:"yellow" , href:"#likes"})
+            break;
+        case "#profile" :
+            render({title:"PROFILE" , color:"green" , href:"#profile"})
+            break;
+    }
+}
+
+function render(data){
+
+    document.querySelector("h1").innerText= data.title;
+    document.querySelector("main").style.backgroundColor = data.color;
+    // document.querySelectorAll("a").forEach(element => {
+    //     if(element.href.includes(data.href)){ 
+    //         element.style.backgroundColor = "black"
+    //     }else{
+    //         element.style.back = "#666"
+    //     }
+    //     });
+
+}
+
